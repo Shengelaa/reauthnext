67fbdd7a7bb60401bac5922a59e47ece0da09770
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2024-11-04 22:39:16 +0400

    rr

--- app/(logged-out)/login/page.tsx
+++ app/(logged-out)/login/page.tsx
@@ -130,7 +130,7 @@ export default function Login() {
           </div>
           <div className='text-sm text-muted-foreground'>
             Forget your password?{"       "}
-            <Link className='underline' href='/reset-password'>
+            <Link className='underline' href='/password-reset'>
               Reset Password
             </Link>
           </div>
