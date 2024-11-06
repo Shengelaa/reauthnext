04b9c2356eb48d7311bbfc2550e820203f5d483e
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2024-11-06 20:17:21 +0400

    feature/resetlink

--- app/(logged-out)/password-reset/action.ts
+++ app/(logged-out)/password-reset/action.ts
@@ -6,6 +6,7 @@ import { passwordResetTokens } from "@/db/passwordResetTokensSchema";
 import { users } from "@/db/userSchema";
 import { randomBytes } from "crypto";
 import { eq } from "drizzle-orm";
+import { mailer } from "@/lib/email";
 
 export const passwordReset = async (emailAddress: string) => {
   const session = await auth();
@@ -46,4 +47,6 @@ export const passwordReset = async (emailAddress: string) => {
         tokenExpiry,
       },
     });
+
+  const resetLink = `${process.env.SITE_BASE_URL}/update-password?token=${passwordResetToken}`;
 };
